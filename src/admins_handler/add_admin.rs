f97a9d280d51d46c6c8414f6233c001da85344ed
IsaacJReay <isaacjacksonreay@gmail.com>
2022-10-30 20:30:01 +0700

    added function to add admin from ROOT

--- src/admins_handler/add_admin.rs
+++ src/admins_handler/add_admin.rs
@@ -1,12 +1,16 @@
 use super::{
-    error, get_value_mutex_safe, post, tbl_admins_handler, validate_token, web, Claims, Error,
-    FromStr, HttpRequest, HttpResponse, LoginModel, LoginRole,
+    error, post, tbl_admins_handler, validate_token, web, AdminsInfo, Error, HttpRequest,
+    HttpResponse, LoginRole,
 };
 
-#[post("/private/api/admins/add")]
-pub async fn add_admins(req: HttpRequest, arg: web::Json<LoginModel>) -> Result<HttpResponse, Error> {
-    let role = match validate_token(&req) {
-        Ok(role) => Ok(role),
+#[post("/private/api/admin/add")]
+pub async fn add_admin(
+    req: HttpRequest,
+    arg: web::Json<AdminsInfo>
+) -> Result<HttpResponse, Error> {
+
+    let (role, claims) = match validate_token(&req) {
+        Ok((role, claims)) => Ok((role, claims)),
         Err((code, message)) => match code {
             401 => Err(actix_web::error::ErrorGone(message)),
             _ => Err(actix_web::error::ErrorUnauthorized(message)),
@@ -15,8 +19,42 @@ pub async fn add_admins(req: HttpRequest, arg: web::Json<LoginModel>) -> Result<
 
     match role {
         LoginRole::Root => Ok(()),
-        _ => Err(actix_web::error::ErrorUnauthorized(String::from("Unauthorised user")))
+        _ => Err(error::ErrorUnauthorized(String::from("Unauthorised user"))),
     }?;
+
+    match tbl_admins_handler::query_existence_of_admin(claims.get_aud()) {
+        true => Ok(()),
+        false => Err(error::ErrorInternalServerError(String::from(
+            "This Root doesn't exists",
+        )))
+    }?;
+
+    match tbl_admins_handler::query_existence_of_admin(&arg.username) {
+        true => Err(error::ErrorInternalServerError(String::from(
+            "User already exists",
+        ))),
+        false => Ok(()),
+    }?;
+
+    if let None = arg.display_name  {
+        Err(error::ErrorBadRequest(String::from("Missing Display Name")))
+    }
+    else if let None = arg.role {
+        Err(error::ErrorBadRequest(String::from("Missing Role")))
+    }
+    else if let None = arg.password {
+        Err(error::ErrorBadRequest(String::from("Missing Password")))
+    }
+    else {
+        Ok(())
+    }?;
+
+    tbl_admins_handler::insert_into_tbl_admins(
+        &arg.display_name.to_owned().unwrap(),
+        &arg.username,
+        &bcrypt::hash::<String>(arg.password.to_owned().unwrap(), bcrypt::DEFAULT_COST).unwrap(),
+        &arg.role.as_ref().to_owned().unwrap().to_string(),
+    );
 
     Ok(HttpResponse::Ok().finish())
 }
